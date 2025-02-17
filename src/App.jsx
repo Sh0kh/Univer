03ee9bfc89db428ca5ff02a30c3c7b4f21ecb515
davlatberdinazarov <aberdinazarov71@gmail.com>
2025-02-17 19:33:44 +0500

    added new page notfound

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import AppLayout from "./layouts/AppLayout";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./Pages/Home";
 import { Helmet, HelmetProvider } from "react-helmet-async"; // Добавлено для SEO
-import './Style/Media.css';
+import "./Style/Media.css";
 import AboutUs from "./Pages/AboutUs";
 import Rahbariyat from "./Pages/Rahbariyat";
 import Partners from "./Pages/Partners";
@@ -12,37 +12,38 @@ import International from "./Pages/International";
 import Documents from "./Pages/Documents";
 import InspectDocument from "./Pages/InspectDocument";
 import OpenInfo from "./Pages/OpenInfo";
+import ProtectedRoute from "./Components/ProtectedRoute";
+import AdminLayout from "./layouts/AdminLayout";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
     <HelmetProvider>
       <Router>
-
-
         <Routes>
           {/* hehe */}
           {/* <Route path="/login" element={<Login />} /> */}
           <Route path="/" element={<AppLayout />}>
-            {/* <Route
+            <Route
+              path="admin"
               element={
                 <ProtectedRoute>
                   <AdminLayout />
                 </ProtectedRoute>
               }
-            >
-            </Route> */}
+            ></Route>
             <Route element={<MainLayout />}>
               <Route index element={<Home />} />
               <Route path="/aboutus" element={<AboutUs />} />
               <Route path="/staff" element={<Rahbariyat />} />
-
-              <Route path='/partners' element={<Partners />} />
-              <Route path='/International' element={<International />} />
+              <Route path="/partners" element={<Partners />} />
+              <Route path="/International" element={<International />} />
               <Route path="/documents" element={<Documents />} />
               <Route path="/inspectDocument" element={<InspectDocument />} />
               <Route path="/openInfo" element={<OpenInfo />} />
             </Route>
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </HelmetProvider>
